47ddb264694ef1d28b1c75c1c7e4a8c6814be57e
Lisandro <lisandroasuvercase@gmail.com>
2022-05-15 20:29:47 -0300

    Guía de ejercicios Python

---
+++ clases/GuiaEj2.py
@@ -0,0 +1,9 @@
+#Ejercicio 1
+#Crear un programa que pida al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
